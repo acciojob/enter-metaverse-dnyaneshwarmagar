5fe7e1ae5085d186004b2551ca6cfc3e39513cfd
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-01 01:24:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let textElement = document.getElementById("status");
+let btnElement = document.getElementById("enterBtn");
+
+function changeText(){
+	textElement.innerHTML = "<h1>Entered Metaverse<h1>"
+}
+
+btnElement.addEventListener("click",changeText)
